e4ef461a506e2afc2a64f8727cbbe6b866cbf640
Rahul Sharma <rahulrs0029@gmail.com>
2022-03-08 16:45:47 +0530

    Initial Commit

---
+++ ChartsSwiftUI/ChartsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsSwiftUIApp.swift
+//  ChartsSwiftUI
+//
+//  Created by Rahul Sharma on 08/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ChartsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
